54aaf5ccdc1e45497b3af2dd6d955a0a37d6b5fc
qq18373459487 <2306732673@qq.com>
2022-04-11 10:26:04 +0800

    添加了EMPATY，InHotel，ListHome,OutHotel,在Mainrun添加了一个getroom方法

--- src/main/java/ListHome.java
+++ src/main/java/ListHome.java
@@ -1,13 +1,24 @@
 public class ListHome {
-    IData iData;
-    public ListHome(IData iData){
-        this.iData = iData;
-    }
+    MainRun mr=new MainRun();
 
     public ListHome() {
     }
+    public ListHome(IData iData) {
+    }
 
     public void search() {
-        iData.search();
-    }
-}
+        for (int i=0;i<mr.getRoom().length;i++){
+            for (int j=0;j<mr.getRoom()[0].length;j++){
+                if(j+1<10){
+                    System.out.println(i+1+"0"+(j+1)+"");
+                }else {
+                    System.out.println(i+1+""+(j+1)+"");
+                };
+            }
+            System.out.println();
+            for(int j=0;j<mr.getRoom()[0].length;j++){
+                System.out.println(mr.getRoom()[i][j]+"   ");
+            }
+            System.out.println();
+        }
+}}
